fb3cb4dc0cdc2250b656655cc021f848608c7424
jason12119 <skvarcekp@gmail.com>
2021-09-16 15:44:06 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project 1</title>
+    <meta name="description" content="My first project after a year in VS Code">
+    <link rel="stylesheet" href="dist/css/frameworks.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="dist/js/frameworks-min.js"></script>
+</head>
+<body>
+    <div class="flexy">
+    <h1>Project 1 - Background Changer</h1>
+        <button id="colorChanger">Change background!</button>
+    </div>
+       
+    <script src="js/functions.js"></script>
+</body>
+</html>
